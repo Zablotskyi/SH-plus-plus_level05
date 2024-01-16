cdae56f989d995085780f4a4d49ba654d66dde4d
Zablotskyi <zablotskyivv@gmail.com>
2024-01-16 15:53:49 +0200

    added a main class

---
+++ src/com/shpp/p2p/cs/vzablotskyi/assignment5/Assignment5Part1.java
@@ -0,0 +1,11 @@
+package com.shpp.p2p.cs.vzablotskyi.assignment5;
+
+public class Assignment5Part1 {
+    public static void main(String[] args) {
+
+    }
+    private int syllablesInWord(String word) {
+
+        return 0;
+    }
+}
